896606df811fe5a86a9cc740297ee79e309b1713
Nilesh Virkar <nilesh.virkar@acellere.com>
2019-12-04 21:20:27 +0530

    Update FileSourceTest.java

--- maven-builder-support/src/test/java/org/apache/maven/building/FileSourceTest.java
+++ maven-builder-support/src/test/java/org/apache/maven/building/FileSourceTest.java
@@ -27,6 +27,7 @@ import java.util.Scanner;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 public class FileSourceTest
 {
